22edc84fb61a12efed6b48860a8c6cc8072f5734
Zana H <zhertica@gmail.com>
2020-05-24 03:09:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -28,4 +28,5 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   planeteer_calls.find do |call|
     call == valid_calls[]
+  end
 end
