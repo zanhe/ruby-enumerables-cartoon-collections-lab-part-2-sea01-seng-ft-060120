301685c40d1d0d6d2bd381b6347d883cc12c6fed
Zana H <zhertica@gmail.com>
2020-05-24 03:02:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ end
 def summon_captain_planet(planeteer_calls)
   # Use an Enumerable to capitalize and add '!' to every element in the passed in array
   # Return a new array of the results
-  planeteer_calls.each do |element|
+  planeteer_calls.collect do |element|
     element.capitalize  + "!"
   end
 end
