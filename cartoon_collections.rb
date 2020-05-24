d8297aafb4766aad6c2408afbc3c111d2256ade1
Zana H <zhertica@gmail.com>
2020-05-24 03:04:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,6 +17,7 @@ end
 def long_planeteer_calls(planeteer_calls)
   # Use an Enumerable to check if any string in the passed in array is greater than 4 characters long
   # Return the boolean result of this check
+  planeteer_calls.any
 end
 
 def find_valid_calls(planeteer_calls)
